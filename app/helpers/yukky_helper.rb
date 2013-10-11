bf8230774f88f0943599ad2df18fa6302d7436fe
Yosuke OTA <yota@ns.ie.u-ryukyu.ac.jp>
2013-10-12 01:52:26 +0900

    Update app/helpers/yukky_helper.rb Add JsonManager class

--- app/helpers/yukky_helper.rb
+++ app/helpers/yukky_helper.rb
@@ -1,2 +1,30 @@
 module YukkyHelper
+  class JsonManager
+    PATH = "./tmp/"
+
+    def initialize(name)
+      @filename = PATH + name
+      @json_data = Array.new
+
+      if File.exist?(@filename) then
+        open(@filename,'r') do |fp|
+          @json_data = JSON.parse(fp.read)
+        end
+      end
+    end
+
+    def get_all
+      return @json_data
+    end
+
+    def add(data)
+      @json_data.unshift data
+    end
+
+    def save
+      open(@filename,"w") do |io|
+        JSON.dump(@json_data,io)
+      end
+    end
+  end
 end
